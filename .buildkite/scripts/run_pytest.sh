faed4ea5c2675f29880e0ba678ddd0802436b4a5
Yafen Fang <yafen.fang@inf.ethz.ch>
2023-02-18 22:28:29 +0000

    install python3.8

--- .buildkite/scripts/run_pytest.sh
+++ .buildkite/scripts/run_pytest.sh
@@ -12,13 +12,13 @@ upgrade_python() {
     if [ ! -d "/root/Python-3.8.12" ]; then
         tar -xvf /root/Python-3.8.12.tgz -C /root
     fi
-    cd /root/Python-3.8.12 && ./configure --enable-optimizations --prefix=/usr && make altinstall && cd -
-    update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
+    cd /root/Python-3.8.12 && ./configure --enable-optimizations --prefix=/opt/conda && make altinstall && cd -
+    update-alternatives --install /opt/conda/bin/python3 python3 /opt/conda/bin/python3.8 1
+    update-alternatives --install /opt/conda/bin/python python /opt/conda/bin/python3.8 1
 }
 
 check_python_version() {
-    PYTHON_VERSION_OK=$(python3 -c 'import sys; print(int(sys.version_info > (3, 7)))')
-    echo ----$PYTHON_VERSION_OK
+    PYTHON_VERSION_OK=$(python3 -c 'import sys; print(int(sys.version_info >= (3, 8)))')
     if [[ $PYTHON_VERSION_OK -eq 0 ]]; then
          upgrade_python
     fi
